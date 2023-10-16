b57a4ea4cfafaf384eb614d4ec25efa660b26309
In Hwan Hwang <91361439+kok05206@users.noreply.github.com>
2023-10-16 14:42:31 +0900

    Create
    지금 알게 될 것
    - 리액트는 사용자 정의 태그를 만드는 기술입니다.
    - Props는 컴포넌트의 외부에서 사용하는 입력값입니다.
    - State는 컴포넌트 내부에서 사용하는 값입니다.
    - 이벤트는 Props를 이용합니다.
    - event.preventDefault()를 이용해서 기본동작을 비활성화 할 수 있습니다.
    - form을 변경할 때는 Props를 State로 전환 후 onChange를 이용합니다.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.svg'; // eslint-disable-line no-unused-vars
 import './App.css';
 import { useState } from 'react';
 
@@ -53,17 +53,44 @@ function Article(props) {
     </article>
   );
 }
+function Create(props) {
+  return (
+    <article>
+      <h2>Create</h2>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const title = event.target.title.value;
+          const body = event.target.body.value;
+          props.onCreate(title, body);
+        }}
+      >
+        <p>
+          <input type='text' name='title' placeholder='title' />
+        </p>
+        <p>
+          <textarea name='body' placeholder='body'></textarea>
+        </p>
+        <p>
+          {' '}
+          <input type='submit' value='Create'></input>
+        </p>
+      </form>
+    </article>
+  );
+}
 function App() {
   // const _mode = useState('WELCOME');
   // const mode = _mode[0];
   // const setMode = _mode[1];
   const [mode, setMode] = useState('WELCOME'); // 위의 3줄 코드와 동일한 작동.
   const [id, setId] = useState(null);
-  const topics = [
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
     { id: 1, title: 'html', body: 'html is...' },
     { id: 2, title: 'css', body: 'css is...' },
     { id: 3, title: 'javascript', body: 'javascript is...' },
-  ];
+  ]);
   let content = null;
   if (mode === 'WELCOME') {
     content = <Article title='Welcome' body='Hello, Web'></Article>;
@@ -78,6 +105,20 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>;
+  } else if (mode === 'CREATE') {
+    content = (
+      <Create
+        onCreate={(_title, _body) => {
+          const newTopic = { id: nextId, title: _title, body: _body };
+          const newTopics = [...topics];
+          newTopics.push(newTopic);
+          setTopics(newTopics);
+          setMode('REAd');
+          setId(nextId);
+          setNextId(nextId + 1);
+        }}
+      ></Create>
+    );
   }
   return (
     <div>
@@ -95,6 +136,15 @@ function App() {
         }}
       ></Nav>
       {content}
+      <a
+        href='/create'
+        onClick={(event) => {
+          event.preventDefault();
+          setMode('CREATE');
+        }}
+      >
+        Create
+      </a>
     </div>
   );
 }
