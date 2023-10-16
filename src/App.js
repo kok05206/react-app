8ebca037182b9b524b1afb30ad369fe2a2ddead1
In Hwan Hwang <91361439+kok05206@users.noreply.github.com>
2023-10-16 13:53:27 +0900

    State
    prop과 state의 차이
    prop은 컴포넌트를 사용하는 외부자를 위한 데이터
    state는 컴포넌트를 만드는 내부자를 위한 데이터다.
    지금 알게 될 것
    - 리액트는 사용자 정의 태그를 만드는 기술입니다.
    - Props는 컴포넌트의 외부에서 사용하는 입력값입니다.
    - State는 컴포넌트 내부에서 사용하는 값입니다.
    - 이벤트는 Props를 이용합니다.
    - event.preventDefault()를 이용해서 기본동작을 비활성화 할 수 있습니다.
    - form을 변경할 때는 Props를 State로 전환 후 onChange를 이용합니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 function Header(props) {
   console.log('props', props, props.title);
   return (
@@ -29,7 +31,7 @@ function Nav(props) {
           href={'/read/' + t.id}
           onClick={(event) => {
             event.preventDefault();
-            props.onChangeMode(event.target.id);
+            props.onChangeMode(Number(event.target.id));
           }}
         >
           {t.title}
@@ -52,26 +54,47 @@ function Article(props) {
   );
 }
 function App() {
+  // const _mode = useState('WELCOME');
+  // const mode = _mode[0];
+  // const setMode = _mode[1];
+  const [mode, setMode] = useState('WELCOME'); // 위의 3줄 코드와 동일한 작동.
+  const [id, setId] = useState(null);
   const topics = [
     { id: 1, title: 'html', body: 'html is...' },
     { id: 2, title: 'css', body: 'css is...' },
     { id: 3, title: 'javascript', body: 'javascript is...' },
   ];
+  let content = null;
+  if (mode === 'WELCOME') {
+    content = <Article title='Welcome' body='Hello, Web'></Article>;
+  } else if (mode === 'READ') {
+    let title,
+      body = null;
+    for (let i = 0; i < topics.length; i++) {
+      console.log(topics[i].id, id);
+      if (topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>;
+  }
   return (
     <div>
       <Header
         title='WEB'
         onChangeMode={() => {
-          alert('Header');
+          setMode('WELCOME');
         }}
       ></Header>
       <Nav
         topics={topics}
-        onChangeMode={(id) => {
-          alert(id);
+        onChangeMode={(_id) => {
+          setMode('READ');
+          setId(_id);
         }}
       ></Nav>
-      <Article title='Welcome' body='Hello, Web'></Article>
+      {content}
     </div>
   );
 }
